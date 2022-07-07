cf91ed36203bd02722a70bc868e8cd6ccf8d05fb
43962 <qianqinggongben@gmail.com>
2022-07-07 04:57:27 +0000

    add

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,6 +30,6 @@ class ListsController < ApplicationController
 
   private
   def list_params
-    params.require(:list).permit(:title,)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
